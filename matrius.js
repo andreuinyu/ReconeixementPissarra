e84af2947f93f6a5553db93005dd9b7fe2e33c7a
Andreu Punsola Soler <andreuinyu@users.noreply.github.com>
2017-10-16 09:50:35 +0200

    Update matrius.js

--- matrius.js
+++ matrius.js
@@ -7,27 +7,27 @@ function Matriu(files, columnes) {
 	this.c = columnes;
 
 	this.matriu = new Array(files);
-	for (var i = 0; i < this.f; i++) {
+	for (let i = 0; i < this.f; i++) {
 		this.matriu[i] = new Array(columnes);
-		for (var j = 0; j < this.c; j++) {
+		for (let j = 0; j < this.c; j++) {
 			this.matriu[i][j] = 0;
 		}
 	}
 
 	Matriu.prototype.aleatoritza =  function(){
-		for(var i = 0; i < this.f; i++) {
-			for(var j = 0; j < this.c; j++) {
+		for(let i = 0; i < this.f; i++) {
+			for(let j = 0; j < this.c; j++) {
 				this.matriu[i][j] = gauss();
-	    	}
+	    		}	
 		}
 	};
 
 	Matriu.prototype.allista = function(){
 		var ll = [];
-		for(var i = 0; i < this.f; i++) {
-			for(var j = 0; j < this.c; j++){
+		for(let i = 0; i < this.f; i++) {
+			for(let j = 0; j < this.c; j++){
 				ll.push(this.matriu[i][j]);
-	    	}
+	    		}
 		}
 		return ll;
 	};
@@ -35,8 +35,8 @@ function Matriu(files, columnes) {
 	Matriu.prototype.tr = function(){
 	//transposada
 		var resultat = new Matriu(this.c, this.f);
-		for(var i = 0; i < resultat.f; i++) {
-			for (var j = 0; j < resultat.c; j++) {
+		for(let i = 0; i < resultat.f; i++) {
+			for (let j = 0; j < resultat.c; j++) {
 				resultat.matriu[i][j] = this.matriu[j][i];
 			}
 		}
@@ -45,8 +45,8 @@ function Matriu(files, columnes) {
 
 	Matriu.prototype.copia = function(){
 		var resultat = new Matriu(this.f, this.c);
-		for(var i = 0; i < this.f; i++) {
-			for(var j = 0; j < this.c; j++) {
+		for(let i = 0; i < this.f; i++) {
+			for(let j = 0; j < this.c; j++) {
 				resultat.matriu[i][j] = this.matriu[i][j];
 	    		}
 		}
@@ -55,14 +55,14 @@ function Matriu(files, columnes) {
 
 	Matriu.prototype.add = function(altre){
 		if (altre instanceof Matriu){
-		    for(var i = 0; i < this.f; i++) {
-				for(var j = 0; j < this.c; j++) {
+		    for(let i = 0; i < this.f; i++) {
+				for(let j = 0; j < this.c; j++) {
 					this.matriu[i][j] += altre.matriu[i][j];
-	    		}
+	    			}
 		    }
 		}else{
-			for(var i = 0; i < this.f; i++) {
-				for(var j = 0; j < this.c; j++) {
+			for(let i = 0; i < this.f; i++) {
+				for(let j = 0; j < this.c; j++) {
 					this.matriu[i][j] += altre;
 	    		}
 		    }
@@ -77,17 +77,17 @@ function Matriu(files, columnes) {
 
 	Matriu.prototype.mul = function(altre){
 		if (altre instanceof Matriu){
-		    for(var i = 0; i < this.f; i++) {
-				for(var j = 0; j < this.c; j++) {
+		    for(let i = 0; i < this.f; i++) {
+				for(let j = 0; j < this.c; j++) {
 				this.matriu[i][j] *= altre.matriu[i][j];
 	    		}
 		    }
 		}else{
-			for(var i = 0; i < this.f; i++) {
-				for(var j = 0; j < this.c; j++) {
+			for(let i = 0; i < this.f; i++) {
+				for(let j = 0; j < this.c; j++) {
 					this.matriu[i][j] *= altre;
-	    		}
-		    }
+	    			}
+		  	}
 		}
 		return this;
 	};
@@ -96,8 +96,8 @@ function Matriu(files, columnes) {
 Matriu.mapa = function(m, func){
 	//Aplica una funció donada a cada valor
 	var resultat = new Matriu(m.f, m.c);
-	for (var i = 0; i < resultat.f; i++) {
-		for (var j = 0; j < resultat.c; j++) {
+	for (let i = 0; i < resultat.f; i++) {
+		for (let j = 0; j < resultat.c; j++) {
 			resultat.matriu[i][j] = func(m.matriu[i][j]);
 		}
 	}
@@ -106,8 +106,8 @@ Matriu.mapa = function(m, func){
 
 Matriu.sub = function(a, b) {
 	var resultat = new Matriu(a.f, a.c);
-	for (var i = 0; i < resultat.f; i++) {
-		for (var j = 0; j < resultat.c; j++) {
+	for (let i = 0; i < resultat.f; i++) {
+		for (let j = 0; j < resultat.c; j++) {
 			resultat.matriu[i][j] = a.matriu[i][j] - b.matriu[i][j];
 		}
 	}
@@ -117,15 +117,15 @@ Matriu.prod = function(a, b){
     //Producte matricial
     if (a.c != b.f){
 		throw "Tamanys Erronis"
-        return;
+      	 	return;
     }
     var resultat = new Matriu(a.f, b.c);
-    for(var i = 0; i < resultat.f; i++) {
-		for(var j = 0; j < resultat.c; j++) {
-            var suma = 0;
-            	for(var k = 0; k < a.c; k++) {
-                	suma += a.matriu[i][k] * b.matriu[k][j];
-				}
+    for(let i = 0; i < resultat.f; i++) {
+		for(let j = 0; j < resultat.c; j++) {
+         		var suma = 0;
+           	 	for(let k = 0; k < a.c; k++) {
+                		suma += a.matriu[i][k] * b.matriu[k][j];
+			}
             	resultat.matriu[i][j] = suma;
 		}
     }
@@ -136,6 +136,6 @@ Matriu.a_matriu = function(ll){
     let m = new Matriu(ll.length, 1);
     for (let i = 0; i < ll.length; i++) {
 		m.matriu[i][0] = parseFloat([i]);
-	}
+    }
     return m;
 }
